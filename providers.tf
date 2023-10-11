ae6450f3fc407b804d87f7b3624e8baec5da60b9
Treviloan <robbienbrandy@gmail.com>
2023-10-11 08:15:06 -0400

    update files

--- providers.tf
+++ providers.tf
@@ -1,12 +1,3 @@
 provider "aws" {
   region  = "us-east-1"
-#  profile = var.profile
-#  region  = var.region-master
-  alias   = "region-master"
 }
-
-#provider "aws" {
-#  profile = var.profile
-#  region  = var.region-worker
-#  alias   = "region-worker"
-#}
